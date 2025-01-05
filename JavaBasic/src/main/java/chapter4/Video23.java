cfa3b865631a632770b3559a483cc9f51cd65050
ThuNguyen3108 <thuthudia123@gmail.com>
2025-01-05 10:13:59 +0700

    #comment class/object

---
+++ JavaBasic/src/main/java/chapter4/Video23.java
@@ -0,0 +1,16 @@
+package chapter4;
+
+public class Video23 {
+    public static void main(String[] args) {
+        System.out.println("Run video 23");
+
+        // qua trinh tao va su dung Object, tu cai truu tuong la class cua chung ta
+        Student st1 = new Student();
+        /* Khai bao 1 object va gán giá trị
+        * */
+        st1.name = "Eric";
+        st1.age = 25;
+
+        System.out.println("Student with name = " + st1.name + " and age = " + st1.age);
+    }
+}
